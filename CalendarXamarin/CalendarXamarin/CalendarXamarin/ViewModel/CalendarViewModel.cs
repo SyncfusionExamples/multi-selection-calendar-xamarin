b394c1e3eea99464c61f2896ad8abdf26b23f44b
Jayaleshwari N <jayaleshwarin@syncfusion.com>
2020-07-02 17:39:23 +0530

    XAMARIN-34939 - multi selection sample added.

---
+++ CalendarXamarin/CalendarXamarin/CalendarXamarin/ViewModel/CalendarViewModel.cs
@@ -0,0 +1,27 @@
+﻿using Syncfusion.SfCalendar.XForms;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Text;
+using Xamarin.Forms;
+
+namespace CalendarXamarin
+{
+    public class CalendarViewModel
+    {
+        public List<DateTime> SelectedDates { get; set; }
+        public CalendarViewModel()
+        {
+            SelectedDates = new List<DateTime>();
+            SelectedDates.Add(new DateTime(2019, 3, 06));
+            SelectedDates.Add(new DateTime(2019, 3, 11));
+            SelectedDates.Add(new DateTime(2019, 3, 15));
+            SelectedDates.Add(new DateTime(2019, 3, 19));
+            SelectedDates.Add(new DateTime(2019, 3, 21));
+            SelectedDates.Add(new DateTime(2019, 3, 25));
+            SelectedDates.Add(new DateTime(2019, 3, 27));
+        }
+    }
+}
+
